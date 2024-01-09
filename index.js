7d1ab22d9af35d39cfa0675527c768f5d8fbfe2f
Ernests Zdanovskis <ernests.zdanovskis2@gmail.com>
2024-01-09 15:53:31 +0200

    skaitītāja programma

---
+++ index.js
@@ -0,0 +1,34 @@
+let number = 0;
+let numberSelector = document.getElementById("number");
+
+function increase() {
+    number++;
+    numberSelector.innerText = number;
+    color()
+}
+
+function reset() {
+    number=0;
+    numberSelector.innerText = number;
+    color()
+}
+
+function decrease() {
+    number--;
+    numberSelector.innerText = number;
+    color()
+}
+
+function color(){
+    if(number<0){
+        numberSelector.style.color="#FE2929"
+    } 
+    else if(number>0){
+        numberSelector.style.color="#6CFF00"
+    }
+    else if(number == 0){
+        numberSelector.style.color="#818282"
+    }
+}
+
+
